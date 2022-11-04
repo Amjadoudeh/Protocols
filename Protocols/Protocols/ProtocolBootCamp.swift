ddcb5c083ea91ff07be6deb6b87bcc633f297a70
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-11-04 12:58:04 +0100

    Create the first protocol

--- Protocols/Protocols/ProtocolBootCamp.swift
+++ Protocols/Protocols/ProtocolBootCamp.swift
@@ -5,6 +5,20 @@ struct DefaultColorTheme {
     let tertiarty: Color = .gray
 }
 
+struct AlternativeColorTheme {
+    let primary: Color = .red
+    let secondary: Color = .white
+    let tertiarty: Color = .green
+}
+
+protocol ColorThemeProtocol {
+    /// The Protocols are set of rules or requirements that struct or class need to have
+    var primary: Color { get }
+    /// we can only get(fetch) its value but we can't change this value - if we want to change it we need to add { get set }
+    var secondary: Color { get }
+    var tertiarty: Color { get }
+}
+
 struct ProtocolBootCamp: View {
     // instance
     let colorTheme: DefaultColorTheme = DefaultColorTheme()
