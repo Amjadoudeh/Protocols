cc8d45369d29836d7b3134fd3cfbfcc0ae114a22
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-11-04 12:24:24 +0100

    first init

---
+++ Protocols/Protocols/ProtocolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolsApp.swift
+//  Protocols
+//
+//  Created by Amjad Oudeh on 04.11.22.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
